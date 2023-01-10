2075411a3ed343a1b273149884d40571b1f1b2b0
miretzky <61268936+gmiretzky@users.noreply.github.com>
2023-01-10 15:26:35 +0200

    Update ipcam_records_backup.ps1

--- ipcam_records_backup.ps1
+++ ipcam_records_backup.ps1
@@ -1,18 +1,20 @@
 #This is the main function for the playback backup utility 
 #Setup some varibles 
-$working_path = "H:\reolink_powershell_scripts"
-$configuration_file = "reolink_playbackup.conf"
+param (
+    [Parameter(Mandatory=$true)][string]$working_path,
+    [Parameter(Mandatory=$true)][string]$configuration_file
+)
 
 #start 
 #Get configuration from file 
 $config = @{}
-foreach ($l in (Get-Content "$($working_path)\$($configuration_file)" | where{$_ -ne "" -and $_ -notlike "#*"}))
+foreach ($l in (Get-Content "$($working_path)\$($configuration_file)" | Where-Object{$_ -ne "" -and $_ -notlike "#*"}))
 {
     $config.Add(($l.split('='))[0].trim(),($l.split('='))[1].trim())
 }
 
 #Get a list of cameras 
-$cameras = (Get-Content -Path "$($working_path)\$($config.camera_list)" | where {$_ -ne "" -and $_ -notlike "#*"})
+$cameras = (Get-Content -Path "$($working_path)\$($config.camera_list)" | Where-Object {$_ -ne "" -and $_ -notlike "#*"})
 
 #Start jobs , First we remove old working jobs 
 Get-Job | Stop-Job
@@ -25,22 +27,22 @@ foreach ($d in 1..$config.days_to_download)
     foreach ($camera in $cameras)
     {
         #get number of working jobs 
-        $running_jobs = Get-Job | where {$_.State -eq "Running"}
+        $running_jobs = Get-Job | Where-Object {$_.State -eq "Running"}
 
         #check to see if we can start another job
         while($running_jobs.count -ge $config.max_jobs){
             Start-Sleep -Seconds $config.jobs_sleep_seconds
             #Check again for free job slot
-            $running_jobs = Get-Job | where {$_.State -eq "Running"}
+            $running_jobs = Get-Job | Where-Object {$_.State -eq "Running"}
         }
         
         #Generate the camera array. 
         $c = $camera.split(',')
         #We have a free job slot, lets call the working script 
         $job_counter += 1
-        $argum ="$($working_path)\workers\$($c[5])\worker.ps1 -camera_ip $($c[0]) -username $($c[1]) -password $($c[2]) -name $($c[3]) -owner $($c[4]) -day_of_backup $($d) -backup_path $($config.backup_folder) -working_path $($working_path)"
+        $argum ="$($working_path)\workers\$($c[5])\worker.ps1 -camera_ip $($c[0]) -username $($c[1]) -password $($c[2]) -name $($c[3]) -owner $($c[4]) -day_of_backup $($d) -backup_path " + '"'+"$($config.backup_folder)"+'" -working_path '+'"'+"$($working_path)"+'"'
         Start-Job -Name "ipcamJob$($job_counter)" -ScriptBlock {
-           Start-Process powershell.exe -ArgumentList $Using:argum -Wait
+           Start-Process pwsh.exe -ArgumentList $Using:argum -Wait
         }
     }
 }
